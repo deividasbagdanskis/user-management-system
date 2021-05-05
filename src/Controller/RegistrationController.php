caed0dc19ec5e3939e7b8cc4882b2c9fa09feed1
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2021-05-05 20:57:47 +0300

    Create registration

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register", name="registration")
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password']
+            ])
+            ->add('register', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $data['password'])
+            );
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirect($this->generateUrl('app_login'));
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
